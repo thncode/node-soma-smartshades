cc91171a2898b7c126be7c85e42d4a37ff00886a
Thomas Nemec <30568242+thncode@users.noreply.github.com>
2019-02-15 22:19:08 +0100

    Update basic.js

--- basic.js
+++ basic.js
@@ -1,18 +1,4 @@
-const MiFlora = require('../index');
+const smartShades = require('SomaShade');
 
-let flora = new MiFlora();
+let shade = new SomaShade();
 
-flora.startScanning();
-
-flora.on('data', function (data) {
-  console.log('data', data);
-});
-
-flora.on('firmware', function (data) {
-  console.log('firmware', data);
-});
-
-setInterval(function () {
-  console.log('every 15 seconds, rescan devices');
-  flora.startScanning();
-}, 15000);
